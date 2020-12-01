baebd696ca1e1cf7179da270025b6580437153c6
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2020-12-01 15:48:32 +0100

    Initial commit

---
+++ src/NativeDraggableList.tsx
@@ -0,0 +1,18 @@
+import { Component, ReactNode, createElement } from "react";
+import { TextStyle, ViewStyle } from "react-native";
+
+import { Style } from "@mendix/pluggable-widgets-tools";
+
+import { HelloWorld } from "./components/HelloWorld";
+import { NativeDraggableListProps } from "../typings/NativeDraggableListProps";
+
+export interface CustomStyle extends Style {
+    container: ViewStyle;
+    label: TextStyle;
+}
+
+export class NativeDraggableList extends Component<NativeDraggableListProps<CustomStyle>> {
+    render(): ReactNode {
+        return <HelloWorld name={this.props.yourName} style={this.props.style} />;
+    }
+}
---
+++ typings/NativeDraggableListProps.d.ts
@@ -0,0 +1,16 @@
+/**
+ * This file was generated from NativeDraggableList.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix UI Content Team
+ */
+export interface NativeDraggableListProps<Style> {
+    name: string;
+    style: Style[];
+    yourName: string;
+}
+
+export interface NativeDraggableListPreviewProps {
+    class: string;
+    style: string;
+    yourName: string;
+}
